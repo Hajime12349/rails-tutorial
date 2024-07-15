7baf115a1da58182385425d445b510bf8607fd67
Hajime12349 <qhqiwmwa@gmail.com>
2024-07-15 13:06:09 +0900

    end section3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get 'foo/bar'
   get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  root "application#hello"
+  get 'static_pages/Contact'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,21 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
   test "should get home" do
     get static_pages_home_url
     assert_response :success
     assert_select "title" , "Home | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get Contact" do
+    get static_pages_Contact_url
+    assert_response :success
+    assert_select "title" , "Contact | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
