e9519721ddd61fd1c198b08b0ec5d4897bc6eede
Hajime12349 <qhqiwmwa@gmail.com>
2024-07-17 13:21:53 +0900

    end 5

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about"
   get "/help", to: "static_pages#help"
   get '/contact', to: "static_pages#contact"
+  get '/signup', to: "users#new"
 end
