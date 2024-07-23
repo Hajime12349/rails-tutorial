caf812530d218c6d32eb0e95bff8829c829f0f16
Hajime12349 <qhqiwmwa@gmail.com>
2024-07-23 12:44:56 +0900

    add test remember

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -30,6 +30,20 @@ class UsersLoginTest < UsersLogin
   end
 end
 
+class RememberingTest < UsersLogin
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
+  end
+end
+
 class InvalidPasswordTest < UsersLogin
 
   test "login path" do
