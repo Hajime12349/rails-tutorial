f3eac4363707ade56006cc27a1e1affc87e8cd62
Hajime12349 <qhqiwmwa@gmail.com>
2024-07-26 20:27:31 +0900

    end 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
 
   def show
     @user=User.find(params[:id])
@@ -31,6 +32,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success]="User deleted"
+    redirect_to users_url, status: :see_other
+  end
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -58,6 +65,10 @@ class UsersController < ApplicationController
     def correct_user
       @user=User.find(params[:id])
       redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url, status: :see_other) unless current_user.admin?
     end
 
 end
