2a94f37f14c9fb869911cfcdab0276daf1d84aad
Hajime12349 <qhqiwmwa@gmail.com>
2024-07-27 23:52:10 +0900

    add test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info]="Please check your email"
+      redirect_to root_url
+      # reset_session
+      # log_in @user
+      # flash[:success] = "Welcome to the Sample App!"
+      # redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
@@ -70,5 +73,4 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_url, status: :see_other) unless current_user.admin?
     end
-
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,19 +2,22 @@ require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference "User.count" do
       post users_path, params:{user: { name:  "",
       email: "user@invalid",
       password:              "foo",
-      password_confirmation: "bar" } }
+      password_confirmation: "bar" }}
     end
+
     assert_response:unprocessable_entity
     assert_template 'users/new'
     assert_select 'div.alert-danger'
     assert_select 'div#error_explanation'
     assert_select 'li'
+
   end
 
   test "valid signup information" do
@@ -25,8 +28,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
-    assert_not flash[:success].empty?
-    assert is_logged_in?
+    # assert_template 'users/show'
+    # assert_not flash[:success].empty?
+    # assert is_logged_in?
   end
 end
